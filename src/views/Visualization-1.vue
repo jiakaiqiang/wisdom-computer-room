128eea74dd86a5f6cbe38d52606772cdb07646aa
jiakaiqiang <1640598618@qq.com>
2024-11-30 12:19:28 +0800

    机房开发

--- src/views/Visualization-1.vue
+++ src/views/Visualization-1.vue
@@ -1,9 +1,9 @@
 <template>
   <div style="height:100%;width:100%">
-    <div ref="thress" class="three-dom" @click="handleClick">
+    <div ref="thress" class="three-dom">
       <div
-          id="plane"
-          :style="{left: state.planePos.left,top:state.planePos.top,display: state.planeDisplay}"
+        id="plane"
+        :style="{left: state.planePos.left,top:state.planePos.top,display: state.planeDisplay}"
       >
         <p>机柜名称：{{ state.curCabinet.name }}</p>
         <p>机柜温度：{{ state.curCabinet.temperature }}°</p>
@@ -24,20 +24,51 @@ import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 
-import * as TWEEN from '@tweenjs/tween.js';
-import {PathGeometry, PathPointList} from "three.path";
+import * as TWEEN from "@tweenjs/tween.js";
+import { PathGeometry, PathPointList } from "three.path";
 const thress = ref(null);
 
-let camera,scene, renderer
-let pathCurve;
-let cameraTween = null;
-let cubPerson;
-let tween
+let camera,
+  scene,
+  renderer,
+  arrow,
+  pathCurve,
+  cameraTween,
+  cubPerson,
+  tween,
+  controls,
+  pathToShow,
+  points;
+let pointArr = [
+  2,
+  0,
+  -5.5,
+  -9,
+  0,
+  -5.5,
+  -9,
+  0,
+  -3,
+  9,
+  0,
+  -3,
+  9,
+  0,
+  2,
+  -5,
+  0,
+  2
+  // 其他坐标点...
+];
+let cabinets = [];
+let curCabinet = null; //划入的机柜
+let curPoint = null //当前点
+let renderFunc = {};
 let jkqSelectObect = {
   material: {}
 };
 let eyesValue = ref(false);
-let controls;
+
 const state = reactive({
   planePos: {
     left: 0,
@@ -52,80 +83,60 @@ const state = reactive({
   }
 });
 
-let arrow = '';
 const raycaster = new THREE.Raycaster();
 const pointer = new THREE.Vector2();
 let maps = new Map();
-let pathToShow;
-let points;
-let pointArr;
-let renderFunc = {};
 
+//创建动画函数
 const registerRenderFunc = (name, func) => {
   renderFunc[name] = func;
 };
 
 const initPathPoints = () => {
-  pointArr = [
-    2, 0, -5.5,
-    -9, 0, -5.5,
-    -9, 0, -3,
-    9, 0, -3,
-    9, 0, 2,
-    -5, 0, 2,
-    // 其他坐标点...
-  ];
-
   points = [];
   for (let i = 0; i < pointArr.length; i += 3) {
-    points.push(new THREE.Vector3(pointArr[i], pointArr[i + 1], pointArr[i + 2]));
+    points.push(
+      new THREE.Vector3(pointArr[i], pointArr[i + 1], pointArr[i + 2])
+    );
   }
 
-  pathCurve = new THREE.CatmullRomCurve3(points, false, 'catmullrom', 0);
-};
-
-const createSquare = () => {
-  const geoPerson = new THREE.BoxGeometry(0.5, 0.5, 0.5);
-  const matPeron = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-  cubPerson = new THREE.Mesh(geoPerson, matPeron);
-  cubPerson.position.set(2, 0.5, -5.5);
-  scene.add(cubPerson);
+  pathCurve = new THREE.CatmullRomCurve3(points, false, "catmullrom", 0);
 };
 
 const moveSquareAlongPath = (points, duration, stopPoints) => {
-  const totalLength = pathCurve.getLength();
-  const step = totalLength / points.length;
+  // const totalLength = pathCurve.getLength();
+  // const step = totalLength / points.length;
 
-  let currentPointIndex = 0;
-  let currentLength = 0;
+  // let currentPointIndex = 0;
+  // let currentLength = 0;
 
-   tween = new TWEEN.Tween({ t: 0 })
+  tween = new TWEEN.Tween({ t: 0 })
     .to({ t: 1 }, duration)
     .easing(TWEEN.Easing.Linear.None)
     .onUpdate(({ t }) => {
-      console.log(t,'sdsdsd')
-      const length = t * totalLength;
+      console.log(t, "sdsdsd");
+      // const length = t * totalLength;
+      //获取对应时间点的信息
       const point = pathCurve.getPointAt(t);
 
       cubPerson.position.copy(point);
       // 更新相机位置和方向
-      camera.position.set(point.x, point.y + 1, point.z); // 相机高度稍微高一点
-      camera.lookAt(point);
+      // camera.position.set(point.x, point.y + 1, point.z); // 相机高度稍微高一点
+      // camera.lookAt(point);
 
-      // 获取前一个点的索引
-      const previousPointIndex = Math.max(0, currentPointIndex - 2);
-      const previousPoint = points[previousPointIndex];
+      // // 获取前一个点的索引
+      // const previousPointIndex = Math.max(0, currentPointIndex - 2);
+      // const previousPoint = points[previousPointIndex];
 
-      // 计算物体的朝向
-      const direction = new THREE.Vector3();
-      direction.subVectors(point, previousPoint).normalize();
+      // // 计算物体的朝向
+      // const direction = new THREE.Vector3();
+      // direction.subVectors(point, previousPoint).normalize();
 
-      // 设置相机的方向
-      camera.quaternion.setFromRotationMatrix(new THREE.Matrix4().lookAt(camera.position, camera.position.clone().add(direction), new THREE.Vector3(0, 1, 0)));
+      // // 设置相机的方向
+      // camera.quaternion.setFromRotationMatrix(new THREE.Matrix4().lookAt(camera.position, camera.position.clone().add(direction), new THREE.Vector3(0, 1, 0)));
 
-
-      currentLength += step;
-      currentPointIndex = Math.floor(currentLength / step);
+      // currentLength += step;
+      // currentPointIndex = Math.floor(currentLength / step);
     })
     .onComplete(() => {
       console.log("路径结束");
@@ -139,7 +150,7 @@ const handleAuto = () => {
   // camera.position.set(0, 1, -5);
   // cubPerson.add(camera);
 
-  const stopPoints = [-9, 0, -5.5,]; // 指定停止点的索引
+  const stopPoints = [-9, 0, -5.5]; // 指定停止点的索引
   const points = pathCurve.getPoints(1000);
   const duration = 12000; // 总时间
 
@@ -156,22 +167,45 @@ const changeEyes = () => {
     controls.update();
   } else {
     camera.position.set(0, 1, -1);
+    camera.lookAt(cubPerson.position);
+
     cubPerson.add(camera);
     controls.enabled = false;
   }
 };
+//加载纹理贴图
+const crtTexture = imgName => {
+  let curTexture = maps.get(imgName);
+  if (!curTexture) {
+    curTexture = new THREE.TextureLoader().load(`public/models/${imgName}.jpg`);
+    curTexture.flipY = false;
+    curTexture.wrapS = 0.5;
+    curTexture.wrapT = 0.5;
+    maps.set(imgName, curTexture);
+  }
+  return curTexture;
+};
+//创建动画效果
+const animate = () => {
+  requestAnimationFrame(animate);
 
+  tween && tween.update(); // 更新 TWEEN 动画
+
+  renderer.render(scene, camera);
+  camera.updateProjectionMatrix();
+};
 const init = () => {
   scene = new THREE.Scene();
   renderer = new THREE.WebGLRenderer();
   initPathPoints();
   createSquare();
   renderPath(renderer);
+  createPoint();
 
   const gltfLoader = new GLTFLoader();
   gltfLoader.load(
     "/public/models/machineRoom.gltf",
-    function (gltf) {
+    function(gltf) {
       gltf.scene.children.forEach(item => {
         const { map, color } = item.material;
         if (map) {
@@ -180,41 +214,34 @@ const init = () => {
             side: THREE.FrontSide // 设置模型只能查看正面
           });
         } else {
-          item.material = new THREE.MeshBasicMaterial({ color, side: THREE.FrontSide });
+          item.material = new THREE.MeshBasicMaterial({
+            color,
+            side: THREE.FrontSide
+          });
+        }
+
+        if (item.name.includes("cabinet")) {
+          cabinets.push(item);
         }
       });
 
-      scene.add(gltf.scene);
-     // scene.add(pathToShow); // 确保路径贴图被添加到场景中
+      scene.add(...gltf.scene.children);
+      // scene.add(pathToShow); // 确保路径贴图被添加到场景中
     },
     num => {},
-    err => {
-      console.log(err, "----wfef");
-    }
+    err => {}
   );
-
-  function crtTexture(imgName) {
-    let curTexture = maps.get(imgName);
-    if (!curTexture) {
-      curTexture = new THREE.TextureLoader().load(`/public/models/${imgName}.jpg`);
-      curTexture.flipY = false;
-      curTexture.wrapS = 0.5;
-      curTexture.wrapT = 0.5;
-      maps.set(imgName, curTexture);
-    }
-    return curTexture;
-  }
 
   camera = new THREE.PerspectiveCamera(
     75,
     thress.value.clientWidth / thress.value.clientHeight,
-    0.1,
+    0.01,
     1000
   );
 
   camera.lookAt(0, 0, 0);
   camera.position.set(0, 20, 4);
-  cubPerson.add(camera);
+  // cubPerson.add(camera);
   scene.add(cubPerson);
 
   // 创建十字坐标
@@ -236,129 +263,80 @@ const init = () => {
 
   controls = new OrbitControls(camera, renderer.domElement);
 
-  controls.addEventListener("change", function () {
+  controls.addEventListener("change", function() {
     renderer.render(scene, camera);
   });
 
-  function animate() {
-    requestAnimationFrame(animate);
-
-    tween&&tween.update(); // 更新 TWEEN 动画
-
-    renderer.render(scene, camera);
-    camera.updateProjectionMatrix();
-  }
-
   animate();
 
-
-
-  renderer.domElement.addEventListener("click", event => {
+  renderer.domElement.addEventListener("dblclick", event => {
     const px = event.offsetX;
     const py = event.offsetY;
-    const x = (px / thress.value.clientWidth) * 2 - 1;
-    const y = -(py / thress.value.clientHeight) * 2 + 1;
+      selectCabinet(px, py);
+      selectPoinet(px, py);
+    if(curCabinet){
+     
+    camera.lookAt(
+      curCabinet.position.x,
+      curCabinet.position.y + 1,
+      curCabinet.position.z
+    );
 
-    raycaster.setFromCamera(new THREE.Vector2(x, y), camera);
+    camera.position.set(
+      curCabinet.position.x,
+      curCabinet.position.y + 1,
+      curCabinet.position.z + 2
+    );
+    controls.target.set(
+      curCabinet.position.x,
+      curCabinet.position.y + 1,
+      curCabinet.position.z
+    );
+    controls.update();
+    controls.enabled = false;
+    }
+    if(curPoint){
 
-    const intersects = raycaster.intersectObjects(scene.children);
-
-    state.planePos.left = px + "px";
-    state.planePos.top = py + "px";
-
-    if (intersects.length > 0) {
-      if (intersects[0].object.name.split("-")[0] === "cabinet") {
-        const selectObject = intersects[0].object;
-        outlinePass.selectedObjects = [selectObject];
-
-        if (Object.keys(jkqSelectObect).length > 0 && jkqSelectObect?.name != selectObject.name) {
-         console.log('wewewe')
-          //
-          // camera.position.set(jkqSelectObect.position.x+1,3,jkqSelectObect.position.z)
-          // camera.lookAt(jkqSelectObect.position.x,3,jkqSelectObect.position.z)
-          //  controls.target.copy(jkqSelectObect.position.x,3,jkqSelectObect.position.z)
-          // controls.update()
-          jkqSelectObect.material.map = crtTexture("cabinet");
-          document.body.style.cursor = "default";
-        } else {
-          document.body.style.cursor = "pointer";
-          selectObject.material.map = crtTexture("cabinet-hover");
-        }
-
-        jkqSelectObect = selectObject;
-      } else {
-
-        jkqSelectObect={}
-      }
-    } else {
-      outlinePass.selectedObjects = [];
-      jkqSelectObect={}
     }
   });
   renderer.domElement.addEventListener("mousemove", event => {
     const px = event.offsetX;
     const py = event.offsetY;
-    const x = (px / thress.value.clientWidth) * 2 - 1;
-    const y = -(py / thress.value.clientHeight) * 2 + 1;
-
-    raycaster.setFromCamera(new THREE.Vector2(x, y), camera);
-
-    const intersects = raycaster.intersectObjects(scene.children);
-
-    state.planePos.left = px + "px";
-    state.planePos.top = py + "px";
-
-    if (intersects.length > 0) {
-      if (intersects[0].object.name.split("-")[0] === "cabinet") {
-        const selectObject = intersects[0].object;
-        outlinePass.selectedObjects = [selectObject];
-
-        if (Object.keys(jkqSelectObect).length > 0 && jkqSelectObect?.name != selectObject.name) {
-          state.planeDisplay = "block";
-          state.curCabinet = {
-            name: `测试机柜001`,
-            temperature: 12,
-            capacity: 20,
-            count: 5
-
-
-          };
-
-
-          document.body.style.cursor = "default";
-        } else {
-          document.body.style.cursor = "pointer";
-
-        }
-
-        jkqSelectObect = selectObject;
-      } else {
-        state.planeDisplay = 'none';
-        jkqSelectObect={}
-      }
-    } else {
-      outlinePass.selectedObjects = [];
-      jkqSelectObect={}
-    }
+    selectCabinet(px, py);
   });
-  renderer.domElement.addEventListener('mouseout',event=>{
-    jkqSelectObect = {}
-  })
+};
+//绘制巡检点
 
+const createSquare = () => {
+  const geoPerson = new THREE.BoxGeometry(0.5, 0.5, 0.5);
+  const matPeron = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+   cubPerson = new THREE.Mesh(geoPerson, matPeron);
+  cubPerson.position.set(2, 0, -5.5);
+  scene.add(cubPerson);
+};
+const createPoint = () => {
+  console.log(points,'wefw')
+  for (let i = 0; i < points.length; i++) {
+    const geoPerson = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+    const matPeron = new THREE.MeshBasicMaterial({ color: "red" });
+    let point = new THREE.Mesh(geoPerson, matPeron);
+    point.position.copy(points[i])
+    scene.add(point);
+  }
 };
 
-const renderPath = async (renderer) => {
-  arrow =  new THREE.TextureLoader().load("public/arrow.png");
+const renderPath = async renderer => {
+  arrow = new THREE.TextureLoader().load("public/arrow.png");
   arrow.wrapS = THREE.RepeatWrapping;
   arrow.anisotropy = renderer.capabilities.getMaxAnisotropy();
 
-  registerRenderFunc('walk-way', () => {
+  registerRenderFunc("walk-way", () => {
     arrow.offset.x -= 0.02;
   });
 
   const material = new THREE.MeshBasicMaterial({
     map: arrow,
-    blending: THREE.CustomBlending,
+    blending: THREE.CustomBlending
   });
 
   const up = new THREE.Vector3(0, 1, 0);
@@ -369,7 +347,7 @@ const renderPath = async (renderer) => {
   const geometry = new PathGeometry();
   geometry.update(pathPoints, {
     width: 0.1,
-    arrow: true,
+    arrow: true
   });
 
   pathToShow = new THREE.Mesh(geometry, material);
@@ -378,24 +356,50 @@ const renderPath = async (renderer) => {
   scene.add(pathToShow);
 };
 
-const selectCabinet = (x, y) => {
-  const { width, height } = thress.value;
-  pointer.set((x / width) * 2 - 1, -(y / height) * 2 + 1);
-};
+const selectCabinet = (px, py) => {
+  const x = (px / thress.value.clientWidth) * 2 - 1;
+  const y = -(py / thress.value.clientHeight) * 2 + 1;
+  //设置鼠标的裁剪坐标和相机的设置射线投射器
+  raycaster.setFromCamera(new THREE.Vector2(x, y), camera);
+  //获取射线投射器和场景中对象的交点
+  const intersect = raycaster.intersectObjects(cabinets)[0];
+  let intersectObj = intersect ? intersect.object : null;
 
-const handleClick = (e) => {
-  console.log(e);
+  //如果当前对象和交叉对象不同 则取消选中状态
+  if (curCabinet && curCabinet !== intersectObj) {
+    curCabinet.material.map = crtTexture("cabinet");
+  }
+  //如果存在交叉对象 则进行选中
+  if (intersectObj) {
+    state.planePos.left = px + "px";
+    state.planePos.top = py + "px";
+    //
+    if (intersectObj !== curCabinet) {
+      curCabinet = intersectObj;
+      intersectObj.material.map = crtTexture("cabinet-hover");
+      state.planeDisplay = "block";
+    }
+    //如果不存在交叉对象 则取消选中状态
+  } else if (curCabinet) {
+    curCabinet = null;
+    state.planeDisplay = "none";
+  }
 };
-
+const  selectPoinet = (px, py) => {
+  const x = (px / thress.value.clientWidth) * 2 - 1;
+  const y = -(py / thress.value.clientHeight) * 2 + 1;
+  //设置鼠标的裁剪坐标和相机的设置射线投射器
+  raycaster.setFromCamera(new THREE.Vector2(x, y), camera);
+}
 onMounted(() => {
   init();
 });
-window.addEventListener('resize', ()=>{
+window.addEventListener("resize", () => {
   renderer.setSize(thress.value.clientWidth, thress.value.clientHeight);
   camera.aspect = thress.value.clientWidth / thress.value.clientHeight;
   camera.updateProjectionMatrix();
- renderPath(renderer);
-})
+  renderPath(renderer);
+});
 </script>
 
 <style scoped lang="scss">
